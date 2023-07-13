b7449371f4d2792a4f62a9c4331c60581f53ace1
jacek Byczkiewicz <jck53416@gmail.com>
2023-07-13 13:54:37 +0200

    Return different trip list every second request

--- app/api/enpoints/transit.py
+++ app/api/enpoints/transit.py
@@ -18,9 +18,10 @@ from app.schemas.vehicle import Vehicle, VehicleEta
 router = APIRouter()
 ride_progress = 0
 ride_increment = 0.05
+cut_trips = True
 
 
-@router.get('/places-collection/{phrase}',
+@router.get('/places-collection/{phrase}/',
             response_model=PlacesCollection)
 def search_location(phrase: str,
                     db: Database = Depends(get_db)):
@@ -29,14 +30,19 @@ def search_location(phrase: str,
     )
 
 
-@router.post('/trips', response_model=list[Trip])
+@router.post('/trips/', response_model=list[Trip])
 def find_trip(origin: str = Form(...),
               destination: str = Form(...),
               db: Database = Depends(get_db)):
+    global cut_trips
+    cut_trips = not cut_trips
+    if cut_trips:
+        return [db.trips[-1]]
+
     return db.trips
 
 
-@router.get('/geometries/routes/{routeId}/start-quay/{quayId}', response_model=FeatureCollection)
+@router.get('/geometries/routes/{routeId}/start-quay/{quayId}/', response_model=FeatureCollection)
 def get_trip_geometry_to_route_end(route_id: uuid.UUID = Path(..., alias='routeId'),
                                    quay_id: str = Path(..., alias='quayId'),
                                    db: Database = Depends(get_db)):
@@ -49,7 +55,7 @@ def get_trip_geometry_to_route_end(route_id: uuid.UUID = Path(..., alias='routeI
     return crud_utils.get_route_geometry(db, route_id=route_id)
 
 
-@router.get('/geometries/routes/{routeId}/start-quay/{startQuayId}/end-quay/{endQuayId}',
+@router.get('/geometries/routes/{routeId}/start-quay/{startQuayId}/end-quay/{endQuayId}/',
             response_model=FeatureCollection)
 def get_trip_geometry(route_id: uuid.UUID = Path(..., alias='routeId'),
                       start_quay_id: str = Path(..., alias='startQuayId'),
@@ -64,7 +70,7 @@ def get_trip_geometry(route_id: uuid.UUID = Path(..., alias='routeId'),
     return crud_utils.get_route_geometry(db, route_id=route_id)
 
 
-@router.get("/vehicles/{vehicleId}/position", response_model=Vehicle)
+@router.get("/vehicles/{vehicleId}/position/", response_model=Vehicle)
 def get_vehicle_position(db: Database = Depends(get_db),
                          vehicle_id: uuid.UUID = Path(..., alias='vehicleId'),
                          ride_simulator: RideSimulator = Depends(RideSimulator)):
@@ -96,21 +102,21 @@ def get_vehicle_position(db: Database = Depends(get_db),
     return vehicle
 
 
-@router.get('/routes/{routeId}', response_model=Route)
+@router.get('/routes/{routeId}/', response_model=Route)
 def get_route_detail(route_id: uuid.UUID = Path(..., alias='routeId')):
     return factories.get_route()
 
 
-@router.get('/cities', response_model=list[City])
+@router.get('/cities/', response_model=list[City])
 def get_cities(db: Database = Depends(get_db)):
     return db.cities
 
 
-@router.get('/quays', response_model=FeatureCollection)
+@router.get('/quays/', response_model=FeatureCollection)
 def get_bus_stops(db: Database = Depends(get_db)):
     return db.bus_stops
 
 
-@router.get('/quays/{quayId}/trips', response_model=list[Trip])
+@router.get('/quays/{quayId}/trips/', response_model=list[Trip])
 def get_trips_for_bus_stop(db: Database = Depends(get_db), quay_id: str = Path(..., alias='quayId')):
     return db.trips
